f3f7416dddc8f5df212ae99a751a7aa2ce319546
tarunsingh-eng <133125226+tarunsingh-eng@users.noreply.github.com>
2026-01-02 18:07:15 +0530

    Modify wait condition in GetTitleTest
    Updated the wait condition to check for URL instead of title.

--- src/test/java/GetTitleTest.java
+++ src/test/java/GetTitleTest.java
@@ -1,11 +1,13 @@
-import java.time.Duration;
-
 import org.junit.Assert;
 import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class GetTitleTest {
 
@@ -13,23 +15,22 @@ public class GetTitleTest {
     public void verifyTitle() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments(
-        "--headless=new",
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu"
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
         );
 
         WebDriver driver = new ChromeDriver(options);
 
-        
         driver.get("http://example.com");
 
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.titleContains("Example"));
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlContains("example"));
 
         String title = driver.getTitle();
-        //Assert.assertTrue(title.contains("example"));
         Assert.assertEquals("Example Domain", title);
+
         driver.quit();
     }
 }
-
