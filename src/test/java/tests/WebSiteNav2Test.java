0bbbf665d8008ecd48cfa297eee356c95c028b36
Tarun Singh <tarun.xpe@gmail.com>
2026-03-18 15:17:08 +0530

    wait updated

--- src/test/java/tests/WebSiteNav2Test.java
+++ src/test/java/tests/WebSiteNav2Test.java
@@ -32,10 +32,10 @@ public class WebSiteNav2Test {
 
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
             driver.get("https://tarunsingh.co.in");
-            WebElement courseLink = driver.findElement(By.linkText("Courses"));
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
-            
+            WebElement courseLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Courses")));
+
             wait.until(ExpectedConditions.elementToBeClickable(courseLink));
             // course click doesn't work - scolls a bit and then stops
             //better alternative is following command : JavascriptExecutor
@@ -74,7 +74,7 @@ public class WebSiteNav2Test {
             JavascriptExecutor js =  (JavascriptExecutor) driver;
             js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
             
-            WebElement projectLink = wait.until((ExpectedConditions.presenceOfElementLocated(By.linkText("Projects"))));
+            WebElement projectLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Projects")));
             
             js.executeScript("arguments[0].scrollIntoView({behavior:'smooth', block:'center'});", projectLink);
             wait.until(ExpectedConditions.elementToBeClickable(projectLink));
